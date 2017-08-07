276d65d8512d882195522ea6cec5dc0c7f1fb846
MichalFaberr <mfaber1995@gmail.com>
2017-08-07 23:09:34 +0200

    .

--- src/main/java/com/Pages/start/impl/Start.java
+++ src/main/java/com/Pages/start/impl/Start.java
@@ -1,11 +1,8 @@
 package com.Pages.start.impl;
 
-import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
-
 import com.vaadin.ui.*;
-
 
 @SpringUI
 //@Theme("dark-flat")
@@ -14,14 +11,31 @@ class Start extends UI{
     private HorizontalLayout _layoutMain;
 
     @Override
-    protected void init(VaadinRequest request) {
+    protected void init(VaadinRequest request)
+    {
         _layoutMain = new HorizontalLayout();
-        this.setContent(_layoutMain);
-        _layoutMain.setSizeFull();
+        _layoutMain.setWidth("100%");
+        _layoutMain.setHeightUndefined();
 
-        _layoutMain.addComponent(new Label("Test..."));
+        Panel panel = new Panel();
+        panel.setSizeFull();
+        panel.setContent(_layoutMain);
+        this.setContent(panel);
+
+        setup();
 
         this.setSizeFull();
+    }
+
+    private void setup()
+    {
+        _layoutMain.addComponent(new Label("Test..."));
+        VerticalLayout layout = new VerticalLayout();
+        _layoutMain.addComponent(layout);
+        for (int i = 0; i<100; ++i)
+            layout.addComponent(new Label("Test..."));
+        _layoutMain.addComponent(new Label("Test..."));
+
     }
 
 
