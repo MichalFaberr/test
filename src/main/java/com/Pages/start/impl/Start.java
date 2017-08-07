89129f95a5f91ad0fc4912405f67fc79e1e90105
MichalFaberr <mfaber1995@gmail.com>
2017-08-07 22:55:08 +0200

    start

--- src/main/java/com/Pages/start/impl/Start.java
+++ src/main/java/com/Pages/start/impl/Start.java
@@ -1,11 +1,5 @@
 package com.Pages.start.impl;
 
-import com.Entities.FlyEntity;
-import com.Entities.GpsEntity;
-import com.Tools.CommunicationTools;
-
-import com.Widgets.MyMap.impl.LeafletMapView;
-import com.Widgets.MyMap.impl.MyMapModel;
 import com.vaadin.annotations.Theme;
 import com.vaadin.server.VaadinRequest;
 import com.vaadin.spring.annotation.SpringUI;
@@ -25,53 +19,10 @@ class Start extends UI{
         this.setContent(_layoutMain);
         _layoutMain.setSizeFull();
 
-        setupLeafletMap();
+        _layoutMain.addComponent(new Label("Test..."));
 
         this.setSizeFull();
     }
 
-    private void setupLeafletMap() {
 
-        try{
-            LeafletMapView leafletMapView = new LeafletMapView(new MyMapModel(1));
-            leafletMapView.setSizeFull();
-
-            Panel panelLeafletMap = new Panel();
-            panelLeafletMap.setContent(leafletMapView);
-            panelLeafletMap.setSizeFull();
-            _layoutMain.addComponent(panelLeafletMap);
-
-        }catch (Exception e){
-            UI.getCurrent().addWindow(new Window("Error", new Label("Start : private void setupLeafletMap() -> leafletMapView exception")));
-        }
-
-    }
-
-    private void setupLayoutTestDataFromRest() {
-
-        _layoutMain.removeAllComponents();
-
-        try{
-
-            _layoutMain.addComponent(new Label("Gps from objects:"));
-
-            for(GpsEntity g : CommunicationTools.getGpsAllIdFly(1)){
-                _layoutMain.addComponent(new Label(g.toString()));
-            }
-
-            _layoutMain.addComponent(new Label("Fly from objects:"));
-
-            for (FlyEntity f : CommunicationTools.getFlyAll() ) {
-                _layoutMain.addComponent(new Label(f.toString()));
-            }
-
-            _layoutMain.addComponent(new Label("Fly by id=2"));
-            _layoutMain.addComponent(new Label(CommunicationTools.getFlyById(2).toString()));
-
-        }catch (Exception e){
-
-        }
-
-
-    }
 }
